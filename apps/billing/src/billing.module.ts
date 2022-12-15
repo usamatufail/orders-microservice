a5226d704ad6b9900c891ed5e92099374d5aa3fb
Usama <miansamusama@gmail.com>
2022-12-16 02:30:29 +0500

    feat(main): added rabbit-mq and billing in docker

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,22 @@
+import { RqmModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
 
 @Module({
-  imports: [],
+  imports: [
+    RqmModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
