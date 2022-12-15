b2ef3aa089f3d4a3f168be41b5ccd724c7a0e4b9
Usama <miansamusama@gmail.com>
2022-12-16 01:44:31 +0500

    feat(orders): orders microservice created with basic endpoints

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,12 @@
 import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import * as joi from 'joi';
 import { OrdersController } from './orders.controller';
+import { OrdersRepository } from './orders.repository';
 import { OrdersService } from './orders.service';
+import { Order, OrderSchema } from './schemas';
 
 @Module({
   imports: [
@@ -13,8 +16,9 @@ import { OrdersService } from './orders.service';
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
